43b76ed8c7a14207dd2128e806ffda3d816a87b1
Leksurion <Leksurion@gmail.com>
2019-02-03 14:40:12 +0300

    Add popup image

---
+++ scripts/show_image.js
@@ -0,0 +1,40 @@
+var showBtns = document.querySelectorAll('.picture-block');
+var image_wrap = document.querySelector('.image-wrap');
+var image_block = image_wrap.querySelector('.image-block');
+var closeBtn = image_wrap.querySelector('.button-close');
+
+//  Set size image in full screen mode
+var setSize = function( winHeight ) {
+    let heightImage = winHeight * 0.9;
+    image_block.style.height = heightImage + 'px';
+    image_block.style.width = (4 * heightImage / 3) + 'px';
+    image_block.style.margin = (winHeight * 0.05) + 'px auto';
+}
+
+var closeImage = function(class_name) {
+    image_wrap.classList.remove('image-wrap-show');
+    image_block.classList.remove( class_name );
+};
+
+setSize( window.innerHeight );
+
+for (let i=0; i<showBtns.length; i++) {
+    var showBtn = showBtns[i];
+    showBtn.addEventListener('click', function(ev) {
+        image_wrap.classList.add('image-wrap-show');
+
+        var img_class = ev.target.getAttribute('img-class');
+        image_block.classList.add( img_class );
+
+        closeBtn.addEventListener('click', function() {
+            closeImage(img_class);
+        });
+        
+        document.addEventListener('keydown', function(ev) {
+            if (ev.keyCode === 27) {
+                closeImage(img_class);
+            }
+        });
+    });
+}
+
